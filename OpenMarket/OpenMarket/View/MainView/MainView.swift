3459003366d5305d4d398db8287d881c25348506
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-03-22 14:14:26 +0900

    refactor: Storyboard ➡️ code 로 변경(3): list <-> grid 변경 시 View를 갈아끼우는 대신 layout을 변경하도록 수정

---
+++ OpenMarket/OpenMarket/View/MainView/MainView.swift
@@ -0,0 +1,99 @@
+//
+//  MainView.swift
+//  OpenMarket
+//
+//  Created by 써니쿠키 on 2023/03/21.
+//
+
+import UIKit
+
+enum MainViewLayoutType {
+
+    case list, grid
+}
+
+final class MainView: UIView {
+
+    private(set) var currentLayoutType: MainViewLayoutType {
+        didSet { layout(for: currentLayoutType) }
+    }
+
+    private(set) var collectionView: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+
+    init (layoutType: MainViewLayoutType = .list) {
+        self.currentLayoutType = layoutType
+        super.init(frame: .zero)
+        configureViewHierarchy()
+        configureViewLayout()
+        layout(for: currentLayoutType)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func changeLayout(to layoutType: MainViewLayoutType) {
+        self.currentLayoutType = layoutType
+    }
+
+    private func layout(for layoutType: MainViewLayoutType) {
+        
+        switch layoutType {
+        case.list:
+            configureListView()
+        case.grid:
+            configureGridLayout()
+        }
+    }
+
+    // ListType
+    private func configureListView() {
+        collectionView.setCollectionViewLayout(createListLayout(), animated: true)
+    }
+
+    private func createListLayout() -> UICollectionViewCompositionalLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+
+    // GridType
+    private func configureGridLayout() {
+        collectionView.setCollectionViewLayout(generateGridLayout(), animated: true)
+    }
+
+    private func generateGridLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(350))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+
+        return layout
+    }
+
+    private func configureViewHierarchy() {
+        self.addSubview(collectionView)
+    }
+
+    private func configureViewLayout() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
--- OpenMarket/OpenMarket/ViewController/MainViewController/MainViewController.swift
+++ OpenMarket/OpenMarket/ViewController/MainViewController/MainViewController.swift
@@ -9,19 +9,107 @@ import UIKit
 
 final class MainViewController: UIViewController {
 
-    lazy private var listView: UIView = {
-        let view: UIView = UIView(frame: self.view.frame)
-        view.translatesAutoresizingMaskIntoConstraints = false
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Page>
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Page>
 
-        return view
-    }()
+    private let mainView: MainView = MainView()
+    private var pageData: [Page] = []
+    private var dataSource: DataSource?
 
-    lazy private var gridView: UIView = {
-        let view: UIView = UIView(frame: self.view.frame)
-        view.translatesAutoresizingMaskIntoConstraints = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        makeBarButton()
+        setUpSegmentControl()
+        fetchMarketData()
+        configureDataSource()
+        applyInitialSnapshot()
+    }
 
-        return view
-    }()
+    private func setupView() {
+        mainView.collectionView.delegate = self
+        self.view = mainView
+        self.view.backgroundColor = .systemBackground
+    }
+
+    private func fetchMarketData() {
+        let marketURLSessionProvider = MarketURLSessionProvider()
+
+        guard let url = Request.productList(pageNumber: 1, itemsPerPage: 100).url else {
+            print(NetworkError.generateUrlFailError)
+            return
+        }
+
+        marketURLSessionProvider.requestDataTask(of: URLRequest(url: url)) { result in
+            switch result {
+            case .success(let data):
+                guard let marketData = JSONDecoder.decodeFromSnakeCase(type: Market.self, from: data) else {
+                    print(NetworkError.dataDecodingFailError.localizedDescription)
+                    return
+                }
+
+                DispatchQueue.main.sync {
+                    self.pageData = marketData.pages
+                    self.applySnapshot()
+                }
+
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    private func configureDataSource() {
+        let listCellRegistration = UICollectionView.CellRegistration<MarketListCell, Page> { (cell, indexPath, page) in
+            cell.configureCell(page: page) { updateConfiguration in
+                if indexPath == self.mainView.collectionView.indexPath(for: cell) {
+                    updateConfiguration()
+                }
+            }
+        }
+
+        let gridCellRegistration = UICollectionView.CellRegistration<MarketGridCell, Page> { (cell, indexPath, page) in
+            cell.configureCell(page: page) { updateImage in
+                if indexPath == self.mainView.collectionView.indexPath(for: cell) {
+                    updateImage()
+                }
+            }
+        }
+
+        dataSource = DataSource(collectionView: mainView.collectionView) {
+            (collectionView, indexPath, page) -> UICollectionViewCell? in
+            switch self.mainView.currentLayoutType {
+            case .list:
+                return collectionView.dequeueConfiguredReusableCell(using: listCellRegistration,
+                                                                    for: indexPath,
+                                                                    item: page)
+            case .grid:
+                return collectionView.dequeueConfiguredReusableCell(using: gridCellRegistration,
+                                                                    for: indexPath,
+                                                                    item: page)
+            }
+        }
+    }
+
+    private func applyInitialSnapshot() {
+        var snapshot = Snapshot()
+
+        snapshot.appendSections([.productList])
+        snapshot.appendItems(pageData)
+        dataSource?.apply(snapshot)
+    }
+
+    private func applySnapshot() {
+        guard let dataSource = dataSource else { return }
+
+        var snapshot = dataSource.snapshot()
+        snapshot.deleteAllItems()
+        snapshot.appendSections([.productList])
+        snapshot.appendItems(pageData)
+        snapshot.reloadSections([.productList])
+
+        dataSource.apply(snapshot)
+    }
 
     lazy private var segmentedControl: UISegmentedControl = {
         let itemsCount = 2
@@ -40,14 +128,7 @@ final class MainViewController: UIViewController {
 
         return segmentControl
     }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        makeBarButton()
-        setUpSegmentControl()
-        self.view = listView
-    }
-    
+
     private func makeBarButton() {
         let button = UIBarButtonItem(image: UIImage(systemName: "plus"),
                                      style: .plain,
@@ -70,13 +151,16 @@ final class MainViewController: UIViewController {
     private func showListView() -> UIAction {
         return UIAction { [weak self] _ in
 
-            self?.view = self?.listView
+            self?.mainView.changeLayout(to: .list)
+            self?.applySnapshot()
         }
     }
 
     private func showGridView() -> UIAction {
         return UIAction { [weak self] _ in
-            self?.view = self?.gridView
+
+            self?.mainView.changeLayout(to: .grid)
+            self?.applySnapshot()
         }
     }
     
@@ -90,5 +174,19 @@ extension MainViewController {
     enum SegmentedItemState: Int {
         case list = 0
         case grid = 1
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let pageId = pageData[indexPath.item].id
+        let detailViewController = DetailViewController(pageId: pageId)
+        self.navigationController?.pushViewController(detailViewController, animated: true)
+    }
+}
+
+extension MainViewController {
+    private enum Section {
+        case productList
     }
 }
